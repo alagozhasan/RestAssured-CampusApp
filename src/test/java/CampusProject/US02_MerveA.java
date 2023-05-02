03f3c73a534bdf8e8643fc6711260f74fd91579f
merve <mervegz263@gmail.com>
2023-05-03 01:54:22 +0300

    Merve Arslan Case ler tamamlandı

--- src/test/java/CampusProject/US02_MerveA.java
+++ src/test/java/CampusProject/US02_MerveA.java
@@ -1,4 +1,154 @@
 package CampusProject;
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class US02_MerveA {
+    Faker faker=new Faker();
+    String  attestationsName;
+    String  attestationsID;
+
+    RequestSpecification recSpec;
+
+
+    @BeforeClass
+    public void Login()  {
+        baseURI="https://test.mersys.io";
+
+        Map<String,String> userCredential=new HashMap<>();
+        userCredential.put("username","turkeyts");
+        userCredential.put("password","TechnoStudy123");
+        userCredential.put("rememberMe","true");
+        Cookies cookies=
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(userCredential)
+
+                        .when()
+                        .post("/auth/login")
+
+                        .then()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies()
+                ;
+
+        recSpec= new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addCookies(cookies)
+                .build();
+    }
+    @Test
+  public void createAttestations(){
+      Map<String,String>attestations=new HashMap<>();
+      attestationsName="Merve Arslan";
+      attestations.put("name",attestationsName);
+
+
+
+
+      attestationsID=
+              given()
+                      .spec(recSpec)
+                      .body(attestations)
+                      .log().body()
+
+                      .when()
+                      .post("/school-service/api/attestation")
+
+                      .then()
+                      .log().body()
+                      .statusCode(201)
+                      .extract().path("id")
+
+      ;
+      System.out.println("attestationsID = " + attestationsID);
+
+  }
+  @Test(dependsOnMethods = "createAttestations")
+    public void createAttestationsNegative(){
+
+      Map<String,String>attestations=new HashMap<>();
+      attestationsName="Merve Arslan";
+      attestations.put("name",attestationsName);
+
+            given()
+                    .spec(recSpec)
+                    .body(attestations)
+                    .log().body()
+
+                    .when()
+                    .post("/school-service/api/attestation")
+
+                    .then()
+                    .log().body()
+                    .statusCode(400)
+                    .body("message",containsString("already"))
+    ;
+
+}
+  @Test(dependsOnMethods = "createAttestationsNegative" )
+  public void updateAttestations(){
+      Map<String,Object>attestations=new HashMap<>();
+     attestations.put("id",attestationsID);
+     attestationsName="Armin Arslan";
+     attestations.put("name",attestationsName);
+              given()
+                      .spec(recSpec)
+                      .body(attestations)
+                      //.log().body()
+                      .when()
+                      .put("/school-service/api/attestation")
+                      .then()
+                      .log().body()
+                      .statusCode(200)
+                      .body("name",equalTo(attestationsName))
+      ;
+  }
+
+  @Test(dependsOnMethods = "updateAttestations")
+  public void deleteAttestations(){
+      given()
+              .spec(recSpec)
+              .pathParam("id",attestationsID)
+              .log().uri()
+              .when()
+              .delete("/school-service/api/attestation/{id}")
+              .then()
+              .log().body()
+              .statusCode(204)
+      ;
+  }
+
+
+
+  @Test(dependsOnMethods = "deleteAttestations")
+  public void deleteAttestationsNegative(){
+      given()
+              .spec(recSpec)
+              .pathParam("id",attestationsID)
+              .log().uri()
+              .when()
+              .delete("/school-service/api/attestation/{id}")
+              .then()
+              .log().body()
+              .statusCode(400)
+              .body("message",equalTo("attestation not found"))
+      ;
+  }
+
+
+
+
+
 }
